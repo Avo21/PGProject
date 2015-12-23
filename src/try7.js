bfcc4e6af9a0114b717ba7b3dec348cddceb5267
Alvaro <alvaro.failde@gmail.com>
2015-12-23 01:31:40 +0100

    Try7 - Follows the try6 approach Adds basic functionality to the main button.

--- src/try7.js
+++ src/try7.js
@@ -1,5 +1,6 @@
 /*
-A different approach
+Try7 - Follows the try6 approach
+Adds basic functionality to the main button.
 */
 
 var gmail_textbox_class = "Am Al editable LW-avf";
@@ -40,7 +41,7 @@ function isEncrypted(text){
 	// TO DO
 }
 
-function createButton_textbox(){
+function createButton_b1(){
 	button = document.createElement("button");
 	button.className = "pgproject_b1"
 	button.type = "button";
@@ -53,18 +54,74 @@ function createButton_textbox(){
     button.style.height = "29px"; //using the 25x25 icon
 
 	if (debug == true){
-		console.log("createButton_textbox returns:");
+		console.log("createButton_b1 returns:");
 		console.log(button);
 	}
 	return button;
 }
 
+// Encrypt button
+function createButton_b2(){
+	button = document.createElement("button");
+	button.className = "pgproject_b2"
+	button.type = "button";
+	button.style.float = "right";
+	button.style.display = "none";
+	//button.style.backgroundColor = color;
+	button.innerText = "Cifrar";
+
+	if (debug == true){
+		console.log("createButton_b2 returns:");
+		console.log(button);
+	}
+	return button;
+}
+
+// Sign button
+function createButton_b3(){
+	button = document.createElement("button");
+	button.className = "pgproject_b3"
+	button.type = "button";
+	button.style.float = "right";
+	button.style.display = "none";
+	//button.style.backgroundColor = color;
+	button.innerText = "Firmar";
+
+	if (debug == true){
+		console.log("createButton_b3 returns:");
+		console.log(button);
+	}
+	return button;
+}
+
+
 function createDiv(){
 	div = document.createElement("div");
 	div.type = "div";
+	div.className = "pgproject_tb_bar"
 
-	button = createButton_textbox();
-	div.appendChild(button);
+	var b1 = createButton_b1();
+	div.appendChild(b1);
+
+	// Encrypt
+	var b2 = createButton_b2();
+	div.appendChild(b2);
+
+	// Sign
+	var b3 = createButton_b3();
+	div.appendChild(b3);
+
+	b1.onclick = function(){
+		if (div.getAttribute("pgproject") != "true") {
+			b2.style.display = "flex";
+			b3.style.display = "flex";
+			div.setAttribute("pgproject", "true");
+		} else {
+			b2.style.display = "none";
+			b3.style.display = "none";
+			div.setAttribute("pgproject", "false");
+		}
+	}
 
 	return div;
 }
@@ -94,6 +151,7 @@ window.addEventListener("click",function(){
 		t = tbTablesList[i];
 		t.style.backgroundColor = color;
 
+		// pgproject indicates when the button already exists in this table
 		if (t.getAttribute("pgproject") != "true") {
 			insertDiv(t);
 			t.setAttribute("pgproject", "true");
